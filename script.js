c54eb341f8b451c0916fdb0ebf6fdba4b3f7def2
Bartholome Hubert <bartholome.hubert@outlook.com>
2025-12-07 11:57:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+emailBox = document.getElementById("email-box")
+
+emailBox.addEventListener("click", e => {
+    emailBox.innerHTML = 'mail<!--- jfdkjfdks -->@<!--- dsjkjsk -->bartholomehubert<!--- ksdlkl --->.<!--- djksjk --->com'
+    emailBox.classList.remove("button-like");
+    emailBox.style.color = "#007e3dff"
+})
