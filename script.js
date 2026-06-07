66369d4182e7ba6cc54e57b6ef0a46a6b238dd8f
Bartholome Hubert <bartholome.hubert@outlook.com>
2026-06-07 10:21:31 -0400

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 emailBox = document.getElementById("email-box")
 
 emailBox.addEventListener("click", e => {
-    emailBox.innerHTML = 'mail<!--- jfdkjfdks -->@<!--- dsjkjsk -->bartholomehubert<!--- ksdlkl --->.<!--- djksjk --->com'
+    emailBox.innerHTML = 'mail<!--- jfdkjfdks --> at <!--- dsjkjsk -->bartholomehubert<!--- ksdlkl ---> dot <!--- djksjk --->com'
     emailBox.classList.remove("button-like");
     emailBox.style.color = "#007e3dff"
 })
